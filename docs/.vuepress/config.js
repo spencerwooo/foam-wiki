b293d0b7b90a92d71953e4e300d4a805077e5852
spencerwooo <spencer.woo@outlook.com>
2020-09-08 21:10:19 +0800

    chore: add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,14 @@ const wikilinks = require('vuepress-markdown-it-wikilink')({
 })
 
 module.exports = {
+  title: 'Spencer\'s Wiki',
+  description: 'Foam as second brain, for adversarial attacks and AI security. Powered by VuePress.',
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+  ],
   theme: path.resolve(__dirname, '../../theme'),
   markdown: {
     extendMarkdown: (md) => {
